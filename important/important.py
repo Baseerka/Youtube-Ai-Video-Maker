5e30228f54e22723722330fa7af0c37f410b1c78
Vinay Viswanath <vinayviswanath04@gmail.com>
2023-04-07 19:14:51 +0530

    added some extra features

--- important/important.py
+++ important/important.py
@@ -3,3 +3,7 @@ openai_api = "YOUR_API_KEY"  #!Important if you want to take facts and images au
 prompt = "20 weird facts each fact is 15 words long or less with different topics and sentences structure, turn them into a Json file like {'facts':{'fact':'', 'topic': ''} and give the topic as a prompt which can be used to generate image about which the fact is talking bout not the catogory but the item itself, don't give anything else other than the dictionary."
 voice = "Matthew" #options = ["Brian","Emma","Russell","Joey","Matthew","Joanna","Kimberly","Amy","Geraint","Nicole","Justin","Ivy","Kendra","Salli","Raveena",]
 #Matthew is my personal favorite :)
+auto_upload = False #Set it to true if you have pasted the file to the folder in the main directory
+manual_upload = False
+browser_data_dir = "user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\Chrome Beta\\User Data\\" #Change it into your own path
+browser_exe_path = "C:\\Program Files\\Google\\Chrome Beta\\Application\\chrome.exe" #Change it into your own path
