1711b147356d8871789c3dd24f9d18ecbcd1fd48
Vinay Viswanath <vinayviswanath04@gmail.com>
2023-04-07 17:24:28 +0530

    Just added YOUR_API_KEY

--- important/important.py
+++ important/important.py
@@ -1,4 +1,4 @@
-openai_api = ""  #!Important if you want to take facts and images automatically
+openai_api = "YOUR_API_KEY"  #!Important if you want to take facts and images automatically
 #used for fact generation !Important (do not change unless you want to make anything else)
 prompt = "20 weird facts each fact is 15 words long or less with different topics and sentences structure, turn them into a Json file like {'facts':{'fact':'', 'topic': ''} and give the topic as a prompt which can be used to generate image about which the fact is talking bout not the catogory but the item itself, don't give anything else other than the dictionary."
 voice = "Matthew" #options = ["Brian","Emma","Russell","Joey","Matthew","Joanna","Kimberly","Amy","Geraint","Nicole","Justin","Ivy","Kendra","Salli","Raveena",]
