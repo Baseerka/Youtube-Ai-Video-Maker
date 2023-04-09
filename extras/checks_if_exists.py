cfbd24583c4184c56e505fe1878dcfc73bfd21d9
Vinay Viswanath <vinayviswanath04@gmail.com>
2023-04-09 17:49:13 +0530

    check if exists updated more effective code

--- extras/checks_if_exists.py
+++ extras/checks_if_exists.py
@@ -12,11 +12,28 @@ def check_if_exists(facts):
     new_facts = []
     if result:
         for fact in facts["facts"]:
-            if fact["fact"] in result["facts"]:
-                continue
-            else:
-                all = {}
-                all["fact"] = fact["fact"]
-                all["topic"] = fact["topic"]
-                new_facts.append(all)
+            for result in result["facts"]:
+                if solve(fact["fact"], result):
+                    continue
+                else:
+                    all = {}
+                    all["fact"] = fact["fact"]
+                    all["topic"] = fact["topic"]
+                    new_facts.append(all)
     return new_facts
+
+def solve(s, t):
+   s1 = s.split()
+   s2 = t.split()
+   if len(s1) > len(s2):
+      s1,s2 = s2,s1
+   while(s1):
+      if(s2[0]==s1[0]):
+         s2.pop(0)
+         s1.pop(0)
+      elif(s2[-1]==s1[-1]):
+         s2.pop()
+         s1.pop()
+      else:
+         return(False)
+   return(True)
