b5d15396345cd9911fd1a839ddc6fb49c5195a38
Vinay Viswanath <vinayviswanath04@gmail.com>
2023-04-11 14:32:26 +0530

    old fact poping up is reduced

--- ai/get_facts_with_title.py
+++ ai/get_facts_with_title.py
@@ -1,13 +1,19 @@
-import openai
+import openai, json
 from important.important import openai_api, prompt
 from ai.json_converter import find_json, get_facts_json
 
 openai.api_key = openai_api
 
+with open("extras/old.json", "r") as old_facts:
+    old_fact = json.load(old_facts)
+    old_fact = "\n".join(old_fact['facts'])
+
 def get_facts():
     messages = []
     system_msg = "You are a helpful assistant who helps me to create a youtube video about facts."
     messages.append({"role": "system", "content": system_msg})
+    old_facts = f"previous facts are \n {old_fact}"
+    messages.append({"role": "assistant", "content": old_facts})
 
     print("Creating facts...")
     message = prompt
@@ -17,9 +23,10 @@ def get_facts():
         messages=messages)
     reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": reply})
+    with open("ai/data/raw.txt", "w") as text:
+        text.write(reply)
     #Saves in case of error
     get_facts_json(reply)
-    #takes dictionary from the given result
     reply = find_json(reply)
     print(reply)
     return reply
--- important/important.py
+++ important/important.py
@@ -1,6 +1,6 @@
 openai_api = "YOUR_API_KEY"  #!Important if you want to take facts and images automatically
 #used for fact generation !Important (do not change unless you want to make anything else)
-prompt = "20 weird facts each fact is 15 words long or less with different topics and sentences structure, turn them into a Json file like {'facts':{'fact':'', 'topic': ''} and give the topic as a prompt which can be used to generate image about which the fact is talking bout not the catogory but the item itself, don't give anything else other than the dictionary."
+prompt = "20 weird facts each fact is 15 words long or less with different topics and sentences structure, don't add any of the old facts,turn them into a Json file like {'facts':{'fact':'', 'topic': ''} and give the topic as a prompt which can be used to generate image about which the fact is talking bout not the catogory but the item itself, don't give anything else other than the dictionary."
 voice = "Matthew" #options = ["Brian","Emma","Russell","Joey","Matthew","Joanna","Kimberly","Amy","Geraint","Nicole","Justin","Ivy","Kendra","Salli","Raveena",]
 #Matthew is my personal favorite :)
 auto_upload = False #Set it to true if you have pasted the file to the folder in the main directory
